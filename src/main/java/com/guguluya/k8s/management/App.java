3235aca7a54504478f9bd30b7e2ff7359986e98e
lzzheng <lzzheng@cn.ibm.com>
2020-06-15 15:39:02 +0800

    init by maven quick-start

---
+++ src/main/java/com/guguluya/k8s/management/App.java
@@ -0,0 +1,13 @@
+package com.guguluya.k8s.management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
