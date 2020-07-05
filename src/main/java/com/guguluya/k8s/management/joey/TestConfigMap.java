d0140d42fba244ec66762117a60713f86101c0b9
zhao jingyu <dlzhaojy@cn.ibm.com>
2020-07-05 08:48:28 +0800

    update config map.

--- src/main/java/com/guguluya/k8s/management/joey/TestConfigMap.java
+++ src/main/java/com/guguluya/k8s/management/joey/TestConfigMap.java
@@ -38,14 +38,10 @@ public class TestConfigMap {
 	}
 
 	private static void createConfigMap() throws ApiException, IOException {
-		ApiClient client = Config.defaultClient();
-		Configuration.setDefaultApiClient(client);
-		CoreV1Api api = new CoreV1Api(client);
-
-		String namespace = "default";
 		V1ConfigMap body = new V1ConfigMap();
 		body.apiVersion("v1");
 		body.kind("ConfigMap");
+
 		Map<String, String> configMapBody = new HashMap<String, String>();
 		configMapBody.put("DEVICE_ID", "A1001");
 		body.data(configMapBody);
@@ -53,21 +49,28 @@ public class TestConfigMap {
 		V1ObjectMeta metadata = new V1ObjectMeta();
 		metadata.setName("test-config-map");
 		body.setMetadata(metadata);
-		api.createNamespacedConfigMap(namespace, body, null, null, null);
+
+		ApiClient client = Config.defaultClient();
+		// Configuration.setDefaultApiClient(client);
+		CoreV1Api api = new CoreV1Api(client);
+		api.createNamespacedConfigMap("default", body, null, null, null);
 	}
 
 	private static void createDeployment() throws ApiException, IOException {
 		Map<String, String> metadataLabels = new HashMap<>();
 		metadataLabels.put("app", "test-deployment");
+
 		V1ConfigMapKeySelector configMapKeyRef = new V1ConfigMapKeySelectorBuilder().withName("test-config-map")
 				.withKey("DEVICE_ID").build();
 		V1EnvVarSource valueFrom = new V1EnvVarSourceBuilder().withConfigMapKeyRef(configMapKeyRef).build();
 		V1EnvVar envVar = new V1EnvVarBuilder().withName("DEVICE_ID").withValueFrom(valueFrom).build();
 		List<V1EnvVar> envVarList = new ArrayList<V1EnvVar>();
 		envVarList.add(envVar);
+
 		V1ContainerPort containerPort = new V1ContainerPortBuilder().withContainerPort(80).build();
 		V1Container container = new V1ContainerBuilder().withName("nginx").withImage("nginx:1.14.2").withEnv(envVarList)
 				.withPorts(containerPort).build();
+
 		V1Deployment body = new V1DeploymentBuilder().withApiVersion("apps/v1").withKind("Deployment").withNewMetadata()
 				.withName("test-deployment").withLabels(metadataLabels).endMetadata().withNewSpec().withNewSelector()
 				.addToMatchLabels("app", "test-deployment").endSelector().withReplicas(1).withNewTemplate()
